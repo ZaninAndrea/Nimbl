bb75bf28cf53e6f5ff2606c26af6a72f69c24d3b
ZaninAndrea <99.zanin@gmail.com>
2017-08-19 18:36:38 +0200

    primitive version of site build

--- src/utilities/buildSite.js
+++ src/utilities/buildSite.js
@@ -1,10 +1,37 @@
 import md from "./markdown-it-conf.js"
-import buildTree from "./buildTree.js"
+import buildTree from "./treeUtils.js"
+import handlebars from "handlebars"
+import yaml from 'js-yaml';
+import path from "path"
 const electron = window.require('electron');
 const fs = electron.remote.require('fs');
 
-const buildSite = dir => {
-    console.log("WORK IN PROGRESS");
+function buildSite(dir){
+    try {
+      var yamlConfig = yaml.safeLoad(fs.readFileSync(path.join(dir,"config.yml"), 'utf8'));
+      console.log(yamlConfig)
+
+      if (!fs.existsSync(path.join(dir,"gh-pages"))){ // if output directory does not exist, create it
+          fs.mkdirSync(path.join(dir,"gh-pages"))
+      }
+
+      for (let i in yamlConfig.map){
+          const source = fs.readFileSync(path.join(dir,yamlConfig.templates.page), 'utf8')
+
+          const mdSource = fs.readFileSync(path.join(dir,yamlConfig.map[i].path), 'utf8')
+          const template = handlebars.compile(source);
+          const context = {title: i, markdownBody: md.render(mdSource), details:yamlConfig.map[i]};
+          const html = template(context);
+          const destination = path.join(dir,"gh-pages",yamlConfig.map[i].path.replace(".md",".html"))
+          console.log(destination);
+          fs.writeFile(destination, html);
+      }
+
+    } catch (e) {
+      console.error(e);
+    }
+
+
 }
 
 export default buildSite
